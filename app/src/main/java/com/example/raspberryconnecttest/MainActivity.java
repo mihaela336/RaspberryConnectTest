67efe64d4e783a535835f5b90a738cb4bda83554
mihaela336 <mihaela.pasca336@gmail.com>
2023-10-05 14:17:48 +0300

    initial commit

---
+++ app/src/main/java/com/example/raspberryconnecttest/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.raspberryconnecttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class MainActivity extends AppCompatActivity {
+    //UI Element
+    Button connect;
+
+    EditText txtAddress;
+    Socket myAppSocket = null;
+    public static String wifiModuleIp = "";
+    public static int wifiModulePort = 0;
+    public static String CMD = "0";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        connect = (Button) findViewById(R.id.connectBtn);
+
+      //  txtAddress = (EditText) findViewById(R.id.ipAddress);
+
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getIPandPort();
+                CMD = "Up";
+                Socket_AsyncTask cmd_increase_servo = new Socket_AsyncTask();
+                cmd_increase_servo.execute();
+            }
+        });
+
+
+    }
+    public void getIPandPort()
+    {
+        String iPandPort = "10.15.10.1:21567";
+        Log.d("MYTEST","IP String: "+ iPandPort);
+        String temp[]= iPandPort.split(":");
+        wifiModuleIp = temp[0];
+        wifiModulePort = Integer.valueOf(temp[1]);
+        Log.d("MY TEST","IP:" +wifiModuleIp);
+        Log.d("MY TEST","PORT:"+wifiModulePort);
+    }
+    public class Socket_AsyncTask extends AsyncTask<Void,Void,Void>
+    {
+        Socket socket;
+
+        @Override
+        protected Void doInBackground(Void... params){
+            try{
+                InetAddress inetAddress = InetAddress.getByName(MainActivity.wifiModuleIp);
+                socket = new java.net.Socket(inetAddress,MainActivity.wifiModulePort);
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeBytes(CMD);
+                dataOutputStream.close();
+                socket.close();
+            }catch (UnknownHostException e){e.printStackTrace();}catch (IOException e){e.printStackTrace();}
+            return null;
+        }
+    }
+}
